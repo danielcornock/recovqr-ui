500828cc1ae036ad080fecefee99809bdb74a70a
Daniel <daniel_cornock@hotmail.co.uk>
2021-06-19 14:02:15 +0100

    refactor(*): move features in to features folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardRoutes } from './dashboard/constants/dashboard-routes.constants';
-import { InformationRoutes } from './information/constants/information-routes.constant';
+import { DashboardRoutes } from './features/dashboard/constants/dashboard-routes.constants';
+import { InformationRoutes } from './features/information/constants/information-routes.constant';
 
 const routes: Routes = [
   {
     path: InformationRoutes.Page,
-    loadChildren: () => import('./information/information.module').then(m => m.InformationModule)
+    loadChildren: () => import('./features/information/information.module').then(m => m.InformationModule)
   },
   {
     path: DashboardRoutes.page,
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
   },
   {
     path: '',
