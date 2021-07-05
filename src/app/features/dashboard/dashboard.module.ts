1541675c45f7b593708c2e088a5719ee9d7f693f
Daniel <daniel_cornock@hotmail.co.uk>
2021-07-05 22:47:12 +0100

    feat(dashboard): display qr code

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppCommonModule } from 'src/app/common/common.module';
 import { AppFormsModule } from 'src/app/shared/forms/forms.module';
 import { InformationLibraryModule } from 'src/app/shared/information-library/information-library.module';
+import { TableModule } from 'src/app/shared/table/table.module';
 import { ContactInformationFormComponent } from './components/contact-information-form/contact-information-form.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
@@ -23,7 +24,8 @@ import { DashboardStore } from './store/dashboard.store';
     DashboardRoutingModule,
     AppCommonModule,
     AppFormsModule,
-    InformationLibraryModule
+    InformationLibraryModule,
+    TableModule
   ],
   providers: [
     DashboardApiService,
--- src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TableOptions } from 'src/app/shared/table/interfaces/table-options.interface';
+import { DashboardApiService } from '../../services/dashboard-api/dashboard-api.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit {
+  public qrCode$: Observable<string>;
+  public isLoading$: Observable<boolean>;
+
+  public tableOptions: TableOptions<unknown>;
+  public items = [];
+
+  constructor(private dashboardApiService: DashboardApiService) {}
+
+  public ngOnInit(): void {
+    this.qrCode$ = this.dashboardApiService.getOwnQrCode().pipe(map((res) => res.qrCode));
+    this.createTable();
+  }
+
+  private createTable(): void {
+    const translationKey = 'DASHBOARD.TAGS.TABLE.';
+
+    this.tableOptions = {
+      columns: [
+        {
+          key: 'location',
+          label: translationKey + 'LOCATION'
+        },
+        {
+          key: 'date',
+          label: translationKey + 'DATE'
+        },
+        {
+          key: 'time',
+          label: translationKey + 'TIME'
+        }
+      ],
+      actions: []
+    };
+  }
 }
