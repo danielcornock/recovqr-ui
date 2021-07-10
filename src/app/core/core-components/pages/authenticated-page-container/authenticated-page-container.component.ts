1c69fec5654ef5ad77d429ea90f3d8629f1eda04
Daniel <daniel_cornock@hotmail.co.uk>
2021-07-10 16:48:50 +0100

    fix(sidebar): ensure sidebar closes on navigation

--- src/app/core/core-components/pages/authenticated-page-container/authenticated-page-container.component.ts
+++ src/app/core/core-components/pages/authenticated-page-container/authenticated-page-container.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { AuthQuery } from 'src/app/core/core-authentication/store/auth.query';
 import { RouterService } from 'src/app/core/core-routing/services/router/router.service';
 import { Maybe } from 'src/types/maybe.type';
@@ -11,18 +12,29 @@ import { Maybe } from 'src/types/maybe.type';
   styleUrls: ['./authenticated-page-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AuthenticatedPageContainerComponent {
+export class AuthenticatedPageContainerComponent implements OnInit {
   public sideMenuOpen = false;
   public userDetails$ = this.authQuery.getUserDetails();
   public pageTitle$: Observable<Maybe<string>>;
 
   constructor(
     private authQuery: AuthQuery,
-    private routerService: RouterService
+    private routerService: RouterService,
+    private router: Router
   ) {
+  }
+  
+  public ngOnInit(): void {
     this.pageTitle$ = this.routerService.getRouteData<{ heading: string }>().pipe(
       map((data) => data.heading)
     );
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart),
+      filter(() => this.sideMenuOpen)
+    ).subscribe(() => {
+      this.sideMenuOpen = false;
+    });
   }
 
   public toggleSideMenu(): void {
