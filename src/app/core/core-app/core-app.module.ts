2d3533aee59fe824fb6b288553aabf84ddd049c2
Daniel <daniel_cornock@hotmail.co.uk>
2021-07-12 21:53:40 +0100

    feat(pwa): enable PWA functionality

--- src/app/core/core-app/core-app.module.ts
+++ src/app/core/core-app/core-app.module.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from 'src/environments/environment';
 import { GeolocationService } from './services/geolocation/geolocation.service';
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ...[environment.production ? [] : AkitaNgDevtools.forRoot()]
+    ...[environment.production ? [] : AkitaNgDevtools.forRoot()],
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [GeolocationService]
 })
