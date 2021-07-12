fcc3fdb96e5997e079cfecb6ba4452427f5e31b2
Daniel <daniel_cornock@hotmail.co.uk>
2021-07-12 18:57:21 +0100

    feat(store): add dev tools module

--- src/app/core/core-app/core-app.module.ts
+++ src/app/core/core-app/core-app.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from 'src/environments/environment';
 import { GeolocationService } from './services/geolocation/geolocation.service';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...[environment.production ? [] : AkitaNgDevtools.forRoot()]
   ],
   providers: [GeolocationService]
 })
